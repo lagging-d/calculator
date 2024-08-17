e38d4eeb68d2c6d3243fd7b07b3dc81fbe9f5e97
Nikiro-Ja <143513350+Nikiro-Ja@users.noreply.github.com>
2024-08-17 16:01:23 +0300

    Feature: check if operator input repeats

--- script.js
+++ script.js
@@ -63,6 +63,9 @@ inputs.addEventListener(`click`, (event) => {
     
     inputHistory.push(event.target.className);
     console.log(inputHistory);
+
+    let checkHistory = inputHistory.map((elem, index) => index !== inputHistory.indexOf(elem) && elem == `oper`);
+    console.log(checkHistory);
 })
 
 clearButton.addEventListener(`click`, () => {
